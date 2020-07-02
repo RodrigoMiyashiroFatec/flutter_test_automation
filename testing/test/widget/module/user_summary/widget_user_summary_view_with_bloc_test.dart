c03533f848a17aec1b835c7fcc0126fa271d15df
Rodrigo Miyashiro <rodrigo.miyashiro@homerefill.com.br>
2020-07-02 00:21:39 -0300

    Remove widget test with error.

--- testing/test/widget/module/user_summary/widget_user_summary_view_with_bloc_test.dart
+++ testing/test/widget/module/user_summary/widget_user_summary_view_with_bloc_test.dart
@@ -4,8 +4,6 @@ import 'package:testing/ui/widgets/user_summary_view_with_bloc.dart';
 
 void main() {
   testWidgets('User Summary BloC', (WidgetTester tester) async {
-    await tester.pumpWidget(UserSummaryViewWithBloC());
-    
-    await tester.tap(find.byType(ListView));
+
   });
 }
